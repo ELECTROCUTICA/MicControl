fe4048d982dcfcabd8ead6773c9a0335f8e89dd0
hua yan <akemi.yanhua@outlook.com>
2023-01-31 18:05:09 +0800

    添加项目文件。

---
+++ WindowsMicControl/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsMicControl {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.麦克风静音ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.显示主窗体ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.设备列表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开机后自启动ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.退出程序ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(213, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 55);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(21, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 14);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "设备列表";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(109, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(263, 52);
+            this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(105, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Test";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "麦克风控制";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.麦克风静音ToolStripMenuItem,
+            this.显示主窗体ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.设备列表ToolStripMenuItem,
+            this.开机后自启动ToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.退出程序ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 126);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // 麦克风静音ToolStripMenuItem
+            // 
+            this.麦克风静音ToolStripMenuItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.麦克风静音ToolStripMenuItem.Name = "麦克风静音ToolStripMenuItem";
+            this.麦克风静音ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.麦克风静音ToolStripMenuItem.Text = "麦克风静音";
+            this.麦克风静音ToolStripMenuItem.Click += new System.EventHandler(this.麦克风静音ToolStripMenuItem_Click);
+            // 
+            // 显示主窗体ToolStripMenuItem
+            // 
+            this.显示主窗体ToolStripMenuItem.Name = "显示主窗体ToolStripMenuItem";
+            this.显示主窗体ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.显示主窗体ToolStripMenuItem.Text = "显示主窗体";
+            this.显示主窗体ToolStripMenuItem.Click += new System.EventHandler(this.显示主窗体ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(145, 6);
+            // 
+            // 设备列表ToolStripMenuItem
+            // 
+            this.设备列表ToolStripMenuItem.Name = "设备列表ToolStripMenuItem";
+            this.设备列表ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.设备列表ToolStripMenuItem.Text = "设备列表";
+            this.设备列表ToolStripMenuItem.Click += new System.EventHandler(this.设备列表ToolStripMenuItem_Click);
+            // 
+            // 开机后自启动ToolStripMenuItem
+            // 
+            this.开机后自启动ToolStripMenuItem.Name = "开机后自启动ToolStripMenuItem";
+            this.开机后自启动ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.开机后自启动ToolStripMenuItem.Text = "开机后自启动";
+            this.开机后自启动ToolStripMenuItem.Click += new System.EventHandler(this.开机后自启动ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(145, 6);
+            // 
+            // 退出程序ToolStripMenuItem
+            // 
+            this.退出程序ToolStripMenuItem.Name = "退出程序ToolStripMenuItem";
+            this.退出程序ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.退出程序ToolStripMenuItem.Text = "退出程序";
+            this.退出程序ToolStripMenuItem.Click += new System.EventHandler(this.退出程序ToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 56);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 156);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "麦克风静音控制";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 显示主窗体ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出程序ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 麦克风静音ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开机后自启动ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设备列表ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsMicControl/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Diagnostics;
+using System.Collections;
+using NAudio.CoreAudioApi;
+using System.Timers;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.IO;
+using IWshRuntimeLibrary;
+using System.Windows.Forms;
+
+namespace WindowsMicControl {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        public static string Developer = "season of yanhua";
+        public static string Version = "1.0";
+        public static bool IsBetaVersion = false;
+
+        public static MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+        public static IEnumerable<MMDevice> micDevice = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToArray();
+
+        public static string ProgramName = "MicControl";
+        public static string MicMutedName;                              //是否静音
+        public static bool RunAfterSystemBoot = false;                  //开机自启状态
+        public static RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);                 //注册表自启键值
+
+        public static Icon Muted = Properties.Resources.muted;                 //静音图标为黑色
+        public static Icon Play = Properties.Resources.play;                   //解除图标为绿色
+        public static Image MainMutedImage = Properties.Resources.MainMuted;
+        public static Image MainPlayImage = Properties.Resources.MainPlay;
+        public static MemoryStream StreamOfMuted = new MemoryStream();
+        public static MemoryStream StreamOfPlay = new MemoryStream();
+
+        public static MMDevice MicroPhone;              //麦克风对象全局变量
+
+        //初始化
+        private void Form1_Load(object sender, EventArgs e) {
+            if (micDevice.Count() == 0) {
+                MessageBox.Show("未找到麦克风设备 \n 请检查麦克风设备是否禁用或拔出", "应用程序错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+
+            for (var i = 0; i < micDevice.Count(); i++) {
+                listBox2.Items.Add(micDevice.ToList()[i]);
+            }
+            MicroPhone = micDevice.ToList()[0];
+            listBox2.SelectedIndex = 0;
+
+            if (MicroPhone.AudioEndpointVolume.Mute == false) {
+                MicMutedName = "否";
+                button1.Text = "麦克风静音";
+                this.notifyIcon1.Icon = Play;
+                this.Icon = Play;
+                this.pictureBox1.Image = MainPlayImage;
+            }
+            else {
+                MicMutedName = "是";
+                button1.Text = "麦克风开启";
+                this.notifyIcon1.Icon = Muted;
+                this.Icon = Muted;
+                this.pictureBox1.Image = MainMutedImage;
+            }
+
+            label1.Text = "静音：" + MicMutedName;
+
+            AppHotKey hotkey = new AppHotKey(Handle);
+            AppHotKey.Hotkey1 = hotkey.RegisterHotkey(Keys.Scroll, AppHotKey.KeyFlags.NONE);
+            hotkey.OnHotkey += new HotkeyEventHandler(PressHotkey);
+
+            ToolStripMenuItem item = new ToolStripMenuItem();
+            item.Text = MicroPhone.ToString();
+            设备列表ToolStripMenuItem.DropDownItems.Add(item);
+            item.Checked = true;
+        }
+
+        //按下热键
+        public void PressHotkey(int HotkeyID) { 
+            if (HotkeyID == AppHotKey.Hotkey1) MuteOrPlay(MicroPhone);
+        }
+
+        //静音/解除
+        public bool MuteOrPlay(MMDevice mmdevice) {
+            if (mmdevice.AudioEndpointVolume.Mute == false) {
+                mmdevice.AudioEndpointVolume.Mute = true;
+                MicMutedName = "是";
+                button1.Text = "麦克风开启";
+                label1.Text = "静音：" + MicMutedName;
+                this.notifyIcon1.Icon = Muted;
+                this.Icon = Muted;
+                this.pictureBox1.Image = MainMutedImage;
+                return true;
+            }
+            else {
+                mmdevice.AudioEndpointVolume.Mute = false;
+                MicMutedName = "否";
+                button1.Text = "麦克风静音";
+                label1.Text = "静音：" + MicMutedName;
+                this.notifyIcon1.Icon = Play;
+                this.Icon = Play;
+                this.pictureBox1.Image = MainPlayImage;
+                return false;
+            }
+        }
+
+        public void DeveloperInfoModify(string developer,string version, bool isbetaversion) { 
+            Form1.Developer = developer;
+            Form1.Version = version;
+            Form1.IsBetaVersion = isbetaversion;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            MuteOrPlay(MicroPhone);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e) {
+            if (this.WindowState == FormWindowState.Minimized) {
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) {
+            if (this.WindowState == FormWindowState.Minimized) {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.Activate();
+            }
+
+        }
+
+        //右键托盘打开菜单后执行
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e) {
+            this.麦克风静音ToolStripMenuItem.Checked = MicroPhone.AudioEndpointVolume.Mute;
+            
+            Play.Save(StreamOfPlay);
+            Image ImaPlay = Image.FromStream(StreamOfPlay);
+            Muted.Save(StreamOfMuted);
+            Image ImaMuted = Image.FromStream(StreamOfMuted);
+
+            if (MicroPhone.AudioEndpointVolume.Mute == false) {
+                this.显示主窗体ToolStripMenuItem.Image = ImaPlay;
+            }
+            else {
+                this.显示主窗体ToolStripMenuItem.Image = ImaMuted;
+            }
+
+            string[] keylist = registryKey.GetValueNames();
+            int index;
+            for (index = 0; index < keylist.Length; index++) {              //循环检索键
+                if (keylist[index].Equals(ProgramName)) {
+                    this.开机后自启动ToolStripMenuItem.Checked = true;
+                    break;
+                }
+            }
+            if (index >= keylist.Length) {
+                this.开机后自启动ToolStripMenuItem.Checked = false;
+            }
+
+        }
+        
+        private void 麦克风静音ToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (this.麦克风静音ToolStripMenuItem.Checked == false) this.麦克风静音ToolStripMenuItem.Checked = true;
+            else this.麦克风静音ToolStripMenuItem.Checked = false;
+            MuteOrPlay(MicroPhone);
+
+        }
+        private void 开机后自启动ToolStripMenuItem_Click(object sender, EventArgs e) {                //开机自启设置
+            if (this.开机后自启动ToolStripMenuItem.Checked == false) {
+                this.开机后自启动ToolStripMenuItem.Checked = true;
+                registryKey.SetValue(ProgramName, Application.ExecutablePath);
+            }
+            else {
+                this.开机后自启动ToolStripMenuItem.Checked = false;
+                registryKey.DeleteValue(ProgramName);
+                
+            }
+            RunAfterSystemBoot = this.开机后自启动ToolStripMenuItem.Checked;
+        }
+
+        private void 设备列表ToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void 显示主窗体ToolStripMenuItem_Click(object sender, EventArgs e) {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+            this.ShowInTaskbar = true;
+        }
+        private void 退出程序ToolStripMenuItem_Click(object sender, EventArgs e) {
+            this.Dispose();
+            Environment.Exit(0);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            notifyIcon1.Dispose();
+        }
+
+        public void StateChangeOnly(MMDevice mmdevice) {
+            if (mmdevice.AudioEndpointVolume.Mute == false) {
+                MicMutedName = "是";
+                button1.Text = "麦克风开启";
+                label1.Text = "静音：" + MicMutedName;
+                this.notifyIcon1.Icon = Muted;
+                this.Icon = Muted;
+                this.pictureBox1.Image = MainMutedImage;
+            }
+            else {
+                MicMutedName = "否";
+                button1.Text = "麦克风静音";
+                label1.Text = "静音：" + MicMutedName;
+                this.notifyIcon1.Icon = Play;
+                this.Icon = Play;
+                this.pictureBox1.Image = MainPlayImage;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+    }
+
+    public delegate void HotkeyEventHandler(int HotKeyID);
+
+    //快捷键类
+    public class AppHotKey : IMessageFilter {
+        public List<UInt32> keyIDs = new List<UInt32>();
+        IntPtr hWnd;
+
+        public static int Hotkey1;
+
+        public event HotkeyEventHandler OnHotkey;
+
+        public enum KeyFlags {
+            NONE = 0,
+            ALT = 1,
+            CONTROL = 2,
+            SHIFT = 4,
+            SCROLL = 8
+        }
+        [DllImport("user32.dll")]
+        public static extern UInt32 RegisterHotKey(IntPtr hWnd, UInt32 id, KeyFlags fsModifiers, UInt32 vk);
+
+        [DllImport("user32.dll")]
+        public static extern UInt32 UnregisterHotKey(IntPtr hWnd, UInt32 id);
+
+        [DllImport("kernel32.dll")]
+        public static extern UInt32 GlobalAddAtom(String lpString);
+
+        [DllImport("kernel32.dll")]
+        public static extern UInt32 GlobalDeleteAtom(UInt32 nAtom);
+
+        public AppHotKey(IntPtr hWnd) {         //构造器
+            this.hWnd = hWnd;
+            Application.AddMessageFilter(this);
+        }
+
+        public int RegisterHotkey(Keys Key, KeyFlags keyflags) {
+            UInt32 hotkeyid = GlobalAddAtom(Guid.NewGuid().ToString());
+            RegisterHotKey(hWnd, hotkeyid, keyflags, (UInt32)Key);
+            keyIDs.Add(hotkeyid);
+            return (int)hotkeyid;
+        }
+
+        public void UnregisterHotkeys() {
+            Application.RemoveMessageFilter(this);
+            foreach (UInt32 key in keyIDs) {
+                UnregisterHotKey(hWnd, key);
+                GlobalDeleteAtom(key);
+            }
+        }
+
+        public bool PreFilterMessage(ref Message m) {
+            if (m.Msg == 0x312) {
+                if (OnHotkey != null) {
+                    foreach (UInt32 key in keyIDs) {
+                        if ((UInt32)m.WParam == key) {
+                            OnHotkey((int)m.WParam);
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
